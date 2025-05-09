bc831d8ee31f089eed7a6d124339bd7deb985d15
christiancmm <chriis.mattos@gmail.com>
2025-05-09 17:08:20 -0300

    alterando alguns arquivos e associando ao banco de dados

--- Js/server.js
+++ Js/server.js
@@ -1,25 +1,43 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
+const db = require('./db')
+
+const app = express()
 const porta = 3000
 
 // volta uma pasta para o express ter acesso as paginas HTML 
 app.use(express.static(path.join(__dirname,'..')))
 
-
-app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','login.html'))
+app.get('/:pagina',(req,res)=>{
+    // coleta a o parametro da URL, definindo qual tela irá abrir
+    const pagina = req.params.pagina
+    res.sendFile(path.join(__dirname,'..',`${pagina}.html`))
 })
 
 //entende a pagina HTML recebida "extended: true" = recebe objetos complexos
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-
 // redireciona a pagina para o index, após fazer o login
-app.post('/',(req,res)=>{
+app.post('/listagem',(req,res)=>{
     console.log(req.body)
-    res.sendFile(path.join(__dirname,'..','index.html'))
+    res.sendFile(path.join(__dirname,'..','listagem.html'))
+})
+
+// Cadastra os itens no banco de dados
+app.post('/enviar',(req, res)=>{
+    // desestrutura os dados enviados ao servidor
+    const { nome, codigo,  quantidade, unidade, preco_custo, preco_venda, fornecedor, descricao} = req.body
+    const sql = 'INSERT into ITENS_CADASTRADOS (  CD_ITEM, TIPO_ITEM, MODELO_ITEM, QUANTIDADE_ITEM, PRECO_ITEM_UN, NOME_FORNECEDOR, DATA_AQUISICAO) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    db.query(sql,[nome, codigo, quantidade, unidade, preco_custo, preco_venda, fornecedor, descricao], (err,result)=>{
+        if (err){
+            console.error('Erro ao inserir no banco', err)
+            res.status(500).send('Erro ao salvar dados')
+        }else{
+            res.send('Dados salvos com sucesso')
+        }
+    })
 })
 
 // porta em que o express escuta
